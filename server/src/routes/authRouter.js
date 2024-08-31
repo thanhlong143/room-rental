ba522a8e12170b5ad65004938abf07353926cf8e
Thanh Long <thanhlongprrt@gmail.com>
2024-09-01 00:04:56 +0700

    server

---
+++ server/src/routes/authRouter.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const router = express.Router()
+
+router.get("/login", (req, res) => {
+   res.send("Ok")
+})
+
+export default router
---
+++ server/src/routes/index.js
@@ -0,0 +1,10 @@
+import authRouter from "./authRouter"
+
+const initRoutes = (app) => {
+   app.use("/api/v1/auth", authRouter)
+   return app.use("/", (req, res) => {
+      res.send("Server on...");
+   })
+}
+
+export default initRoutes
